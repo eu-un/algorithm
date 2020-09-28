f8c6176e0a42d1d63442874a13182b0b458ab105
Cho Eunseo <Cho Eunseo@DESKTOP-PGL3HGC>
2020-09-28 17:58:23 +0900

    LeetCode - 448. Find All Numbers Disappeared in an Array

---
+++ alphago/src/alphago/FindAllNumbers.java
@@ -0,0 +1,24 @@
+package alphago;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindAllNumbers {
+	public List<Integer> findDisappearedNumbers(int[] nums) {
+		List<Integer> result = new ArrayList<Integer>();
+		boolean[] checkArray = new boolean[nums.length + 1]; 
+		
+		for(int i = 0; i < nums.length; i++) {
+			checkArray[nums[i]] = true;
+		} // for end
+		
+		for(int i = 1; i < nums.length; i++) {
+			if(checkArray[i] == false) {
+				result.add(i);
+			} // if end
+		} // for end
+		
+		return result;
+	}
+
+}
