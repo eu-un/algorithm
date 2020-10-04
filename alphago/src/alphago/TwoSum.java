337872d1fef73abef6bd70eaef7989bf1d8ac5d3
Cho Eunseo <Cho Eunseo@DESKTOP-PGL3HGC>
2020-10-04 14:40:18 +0900

    LeetCode - 1. Two Sum

---
+++ alphago/src/alphago/TwoSum.java
@@ -0,0 +1,23 @@
+package alphago;
+
+/*
+	Runtime : 61 ms
+	Memory : 39.1 MB
+*/
+
+public class TwoSum {
+	public static int[] twoSum(int[] nums, int target) {
+		int[] result = new int[2];
+		int num_len = nums.length;
+		
+		for(int i = 0; i < num_len - 1; i++) { // 자기 자신 제외 비교
+			for(int j = i + 1; j < num_len; j++) { // 앞에 비교한 것 비교X
+				if(nums[i] + nums[j] == target) {
+					result[0] = i;
+					result[1] = j;
+				} // if end
+			} // for end
+		} // for end
+		return result;
+	}
+}
